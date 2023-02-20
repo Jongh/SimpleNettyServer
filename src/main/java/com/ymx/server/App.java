7aca47744934502ff7349215c5e98acecc568c57
JongH <kjhfood@hotmail.com>
2023-02-20 14:41:14 +0900

    최초 프로젝트 세팅

---
+++ src/main/java/com/ymx/server/App.java
@@ -0,0 +1,13 @@
+package com.ymx.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
